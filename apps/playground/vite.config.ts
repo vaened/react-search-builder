d7627a17b6143fc6f42486a9e82b85fe06b9efa2
Enea Dhack <enea@vaened.dev>
2025-12-10 16:40:10 -0500

    feat: update build config for mui date pickers support 📅
    Added a path alias for "@vaened/mui-search-builder/dates" in tsconfig. Updated Vite config to include and resolve paths for "@mui/material" and "@mui/x-date-pickers".

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,9 +1,23 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  optimizeDeps: {
+    include: ["@mui/material", "@mui/x-date-pickers"],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+
+      "@mui/material": path.resolve(__dirname, "./node_modules/@mui/material"),
+      "@mui/x-date-pickers": path.resolve(__dirname, "./node_modules/@mui/x-date-pickers"),
+      "@mui/x-date-pickers-pro": path.resolve(__dirname, "./node_modules/@mui/x-date-pickers-pro"),
+    },
+  },
   server: {
     port: 5173,
     open: true,
