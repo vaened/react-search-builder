68238a5e19c5d8c0dfd824271d629ace053abdfa
Enea Dhack <enea@vaened.dev>
2025-12-11 16:01:17 -0500

    refactor: simplify field type usage in utility function
    Streamlined type usage by replacing 'RegisteredField' with 'Field' and 'GenericRegisteredField' with 'GenericField'. This reduces redundancy and aligns the function parameters with updated type definitions.

--- packages/react/src/store/utils.ts
+++ packages/react/src/store/utils.ts
@@ -3,16 +3,15 @@
  * @link https://vaened.dev DevFolio
  */
 
-import { FilterTypeKey, FilterTypeMap, RegisteredField } from "../field";
-import { GenericRegisteredField, RegisteredFieldValue } from "./FieldsRepository";
+import { Field, FilterTypeKey, FilterTypeMap, GenericField } from "../field";
 
-export function isFieldDirty(field: GenericRegisteredField, value: RegisteredFieldValue): boolean;
+export function isFieldDirty(field: GenericField, value: GenericField["value"]): boolean;
 export function isFieldDirty<TKey extends FilterTypeKey, TValue extends FilterTypeMap[TKey]>(
-  field: RegisteredField<TKey, TValue>,
+  field: Field<TKey, TValue>,
   value: TValue | null
 ): boolean;
 export function isFieldDirty<TKey extends FilterTypeKey, TValue extends FilterTypeMap[TKey]>(
-  field: RegisteredField<TKey, TValue>,
+  field: Field<TKey, TValue>,
   value: TValue | null
 ) {
   return !Object.is(field.value, value);
