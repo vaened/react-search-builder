2dea922dc3523ae6f744941a05b93ee5517c631f
Enea Dhack <enea@vaened.dev>
2025-12-11 20:10:09 -0500

    fix: allow null values in isFieldDirty function
    Updated the isFieldDirty function to accept null values for the 'value' parameter. This change ensures the function can handle cases where the value might be null, enhancing its flexibility.

--- packages/react/src/store/utils.ts
+++ packages/react/src/store/utils.ts
@@ -5,7 +5,7 @@
 
 import { Field, FilterTypeKey, FilterTypeMap, GenericField } from "../field";
 
-export function isFieldDirty(field: GenericField, value: GenericField["value"]): boolean;
+export function isFieldDirty(field: GenericField, value: GenericField["value"] | null): boolean;
 export function isFieldDirty<TKey extends FilterTypeKey, TValue extends FilterTypeMap[TKey]>(
   field: Field<TKey, TValue>,
   value: TValue | null
