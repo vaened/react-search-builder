07bba0e7ab9a1476adacbe0a3a68bb4cd1f71c9d
Enea Dhack <enea@vaened.dev>
2025-12-11 21:20:54 -0500

    feat: add validation messages for en and es locales 🌐
    Introduced validation messages for 'required', 'after', 'before', 'length', and 'range'. These messages are now available in both English and Spanish locales, supporting various validation scenarios.

--- packages/mui/src/config/translations.ts
+++ packages/mui/src/config/translations.ts
@@ -31,6 +31,32 @@ export default {
       clearAllTooltip: "Clear all filters",
       clearAllAriaLabel: "delete",
     },
+    validations: {
+      required: {
+        default: "This field is required",
+      },
+      after: {
+        default: "This field must be after {value}",
+      },
+      before: {
+        default: "This field must be before {value}",
+      },
+      length: {
+        default: "This field is invalid",
+        invalid_length_string: "This field must be between {min} and {max} characters",
+        invalid_length_array: "This field must be between {min} and {max} items",
+        invalid_min_length_string: "This field must have at least {min} characters",
+        invalid_min_length_array: "This field must be greater than {min} items",
+        invalid_max_length_string: "This field must be less than {max} characters",
+        invalid_max_length_array: "This field must be less than {max} items",
+      },
+      range: {
+        default: "This field is invalid",
+        invalid_range: "This field must be between minimum {min} and maximum {max}",
+        invalid_min_range: "This field must be greater than {min}",
+        invalid_max_range: "This field must be less than {max}",
+      },
+    },
   },
   es: {
     global: {
@@ -55,6 +81,32 @@ export default {
       noFilters: "No se han aplicado filtros aún.",
       clearAllTooltip: "Borrar todos los filtros",
       clearAllAriaLabel: "borrar",
+    },
+    validations: {
+      required: {
+        default: "Este campo es obligatorio",
+      },
+      after: {
+        default: "Este campo debe ser posterior a {value}",
+      },
+      before: {
+        default: "Este campo debe ser anterior a {value}",
+      },
+      length: {
+        default: "Este campo es inválido",
+        invalid_length_string: "Este campo debe tener entre {min} y {max} caracteres",
+        invalid_length_array: "Este campo debe tener entre {min} y {max} elementos",
+        invalid_min_length_string: "Este campo debe tener al menos {min} caracteres",
+        invalid_min_length_array: "Este campo debe tener al menos {min} elementos",
+        invalid_max_length_string: "Este campo debe tener menos de {max} caracteres",
+        invalid_max_length_array: "Este campo debe tener menos de {max} elementos",
+      },
+      range: {
+        default: "Este campo es inválido",
+        invalid_range: "Este campo debe estar entre minimo {min} y máximo {max}",
+        invalid_min_range: "Este campo debe estar al menos {min}",
+        invalid_max_range: "Este campo debe estar menos de {max}",
+      },
     },
   },
 } satisfies Record<Locale, TranslationStrings>;
