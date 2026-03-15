7d85d92774bc85be93cac425d607dece4cad291c
Enea Dhack <enea@vaened.dev>
2026-03-15 12:26:46 -0500

    refactor: update InputSize type to use InputBaseProps for improved flexibility

--- packages/mui/src/types.d.ts
+++ packages/mui/src/types.d.ts
@@ -3,6 +3,8 @@
  * @link https://vaened.dev DevFolio
  */
 
-export type InputSize = "small" | "medium";
+import type { InputBaseProps } from "@mui/material/InputBase";
+
+export type InputSize = NonNullable<InputBaseProps["size"]>;
 
 export type Locale = "en" | "es";
