d164c8a4c7c44b0053c93d7c39db5197d43a1b83
Enea Dhack <enea@vaened.dev>
2026-03-20 09:14:56 -0500

    refactor: enhance useResolveFieldStoreInstance to utilize default persistence configuration Updated the field store creation logic to incorporate resolveCreateStoreConfigWithDefaultPersistence. This change allows for better handling of persistence options based on the current configuration.

--- packages/react/src/hooks/useResolveFieldStoreInstance.ts
+++ packages/react/src/hooks/useResolveFieldStoreInstance.ts
@@ -4,14 +4,17 @@
  */
 
 import { useRef } from "react";
-import { createFieldStore, CreateStoreOptions, FieldStore } from "../store";
+import { useSearchBuilderConfig } from "../contexts/SearchBuilderConfig";
+import { createFieldStore, CreateStoreOptions, FieldStore, resolveCreateStoreConfigWithDefaultPersistence } from "../store";
 
 export function useResolveFieldStoreInstance(source?: FieldStore, fallback?: CreateStoreOptions): FieldStore {
   const instance = useRef<FieldStore | null>(null);
+  const configuration = useSearchBuilderConfig();
 
   if (source) {
     return source;
   }
 
-  return (instance.current ??= createFieldStore(fallback));
+  return (instance.current ??=
+    createFieldStore(resolveCreateStoreConfigWithDefaultPersistence(fallback, configuration?.createPersistence)));
 }
